93c9ba945ab5b45c3da7ef8c478c1e5d9f398727
Tom Steele <tsteele@madisoncollege.edu>
2020-11-18 17:47:53 -0600

    Hmm...
    http rather than https WTF...

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -51,7 +51,7 @@ export class SwapiService {
 
     // Finally, recurse, use the hypermedia REST API ! ! !
 
-    return this.http.get("https://swapi.dev/api/planets/")
+    return this.http.get("http://swapi.dev/api/planets/")
       .pipe(
         tap(x => console.log(x))
         , expand(x => (x as any).next ? this.http.get((x as any).next) : EMPTY )
